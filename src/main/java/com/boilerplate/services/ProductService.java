1422b052b79b0a6c47180b64df836fae2e6b5844
ratikesh <ratikesh.limetray@gmail.com>
2016-08-01 10:05:26 +0545

    Product Service Complete

--- src/main/java/com/boilerplate/services/ProductService.java
+++ src/main/java/com/boilerplate/services/ProductService.java
@@ -1,10 +1,17 @@
 package com.boilerplate.services;
 
-import com.boilerplate.requests.Product;
+import com.boilerplate.dao.BaseDao;
+import com.boilerplate.domains.Product;
+import com.boilerplate.domains.ProductSKU;
+import com.boilerplate.domains.ProductSKURules;
+import com.boilerplate.requests.ProductSKURule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.xml.transform.Result;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ratikesh on 29/7/16.
@@ -12,12 +19,45 @@ import javax.xml.transform.Result;
 @Service
 public class ProductService {
 
+    @Autowired
+    BaseDao baseDao;
     @Transactional
-    public Product saveProducts(Product product){
+    public Product saveProducts(Product productRequest){
 
-
-
-        return product;
+        Product product = new Product();
+        product.setProductName(productRequest.getProductName());
+        product.setProductId(productRequest.getProductId());
+        product.setProductDescription(productRequest.getProductDescription());
+        product.setAddon(productRequest.isAddon());
+        product.setProductType(productRequest.getProductType());
+        product.setBrandOutletId(productRequest.getBrandOutletId());
+        product.setSourceId(productRequest.getSourceId());
+        List<com.boilerplate.requests.ProductSKU>productSKURequest=new ArrayList<com.boilerplate.requests.ProductSKU>();
+        List<ProductSKU> productSKUList= productRequest.getProductSKUList();
+        for (com.boilerplate.requests.ProductSKU SKU:productSKURequest) {
+            ProductSKU productSKU = new ProductSKU();
+            productSKU.setProductSKUDescription(SKU.getProductSKUDescription());
+            productSKU.setProductSKUName(SKU.getProductSKUName());
+            productSKU.setProductSKUId(SKU.getProductSKUId());
+            productSKU.setProductSKUPrice(SKU.getProductSKUPrice());
+            productSKU.setProductSKUStandardUnit(SKU.getProductSKUStandardUnit());
+            productSKU.setProduct(product);
+            List<ProductSKURules> productSKURulesList = new ArrayList<ProductSKURules>();
+            List<ProductSKURule> productSKURuleRequest = productSKU.getProductSKURulesList();
+            for (ProductSKURule SKURule : productSKURuleRequest) {
+                ProductSKURules productSKURules = new ProductSKURules();
+                productSKURules.setProductSKU(productSKU);
+                productSKURules.setPriceFactor(SKURule.getPriceFactor());
+                productSKURules.setSkuMaxUnit(SKURule.getMaxUnit());
+                productSKURules.setSkuMinUnit(SKURule.getMinUnit());
+                productSKURulesList.add(productSKURules);
+            }
+            productSKU.setProductSKURulesList(productSKURulesList);
+            productSKUList.add(productSKU);
+        }
+        product.setProductSKUList(productSKUList);
+        baseDao.save(product);
+        return productRequest;
     }
 
 }
